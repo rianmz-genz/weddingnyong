d8edf7e0b28ca0d705104340db8bbc4b38f335dc
unknown <adrianji1945@gmail.com>
2023-01-06 09:51:23 +0700

    side position

--- components/home/layout/Container.tsx
+++ components/home/layout/Container.tsx
@@ -3,7 +3,7 @@ import { FC, ReactNode } from "react"
 const Container:FC<ContainerProps> = ({children}) => {
   return (
     <div className="w-full min-h-screen ">
-      <div className="w-11/12 mx-auto h-fit flex">
+      <div className="w-10/12 mx-auto h-fit flex">
         {children}
       </div>
     </div>
